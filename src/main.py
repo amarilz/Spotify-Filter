78b328c130299645650149752d12d06097a934cb
Amarildo Aliaj <82412439+amarildoaliaj@users.noreply.github.com>
2022-10-23 16:19:28 +0200

    refactor: Improved readability + documentation

--- src/main.py
+++ src/main.py
@@ -1,23 +1,17 @@
 import aggiungi_nuova_playlist
 import canzoni_in_playlist_block
-import rimuovi_doppioni
-
-SPOTIPY_CLIENT_ID = '0ca9f8c8577241cc887387b287439978'
-SPOTIPY_CLIENT_SECRET = '8942bd0d3d0848fbba413164d1a24d60'
-PLAYLIST_BLOCK_ID = 'spotify:playlist:7yf9ANeYhjBjQ8jaY2sB0K'
-PLAYLIST_ASCOLTA_ID = '6GVxbCD9eBQFAZJSv9x7ef'
+import duplicates
+from config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, PLAYLIST_BLOCK_ID, PLAYLIST_LISTEN_ID
 
 if __name__ == '__main__':
     canzoni = canzoni_in_playlist_block.get_tracks_from_block_playlist(client_id=SPOTIPY_CLIENT_ID,
                                                                        client_secret=SPOTIPY_CLIENT_SECRET,
                                                                        playlist_id=PLAYLIST_BLOCK_ID)
-
-    # La seguente funzione ritorna uan lista di id di canzoni nuove
-    id_canzoni_nuove_da_mettere_in_playlist = rimuovi_doppioni.controlla_doppioni(canzoni)
+    id_canzoni_nuove_da_mettere_in_playlist = duplicates.remove_listened_songs(canzoni)
 
     aggiungi_nuova_playlist.aggiungi_in_playlist_ascolta(
         id_canzoni_nuove_da_mettere_in_playlist,
         SPOTIPY_CLIENT_ID,
         SPOTIPY_CLIENT_SECRET,
-        PLAYLIST_ASCOLTA_ID
+        PLAYLIST_LISTEN_ID
     )
