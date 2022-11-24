4074be30695b4e0506105a4bc1ec1d0d5f09e9c6
Amarildo Aliaj <82412439+amarildoali@users.noreply.github.com>
2022-11-24 20:22:56 +0100

    refactoring: main

--- src/main.py
+++ src/main.py
@@ -4,10 +4,14 @@ from config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, PLAYLIST_BLOCK_ID, 
 from duplicates import remove_listened_songs
 
 if __name__ == '__main__':
-    songs_in_block_playlist = get_songs_of_block_playlist(client_id=SPOTIPY_CLIENT_ID,
-                                                          client_secret=SPOTIPY_CLIENT_SECRET,
-                                                          playlist_id=PLAYLIST_BLOCK_ID)
-    new_song_ids = remove_listened_songs(songs_in_block_playlist=songs_in_block_playlist)
+    songs_in_block_playlist = get_songs_of_block_playlist(
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        playlist_id=PLAYLIST_BLOCK_ID)
+
+    new_song_ids = remove_listened_songs(
+        songs_in_block_playlist=songs_in_block_playlist)
+
     add_new_songs_in_playlist(
         new_song_ids=new_song_ids,
         client_id=SPOTIPY_CLIENT_ID,
